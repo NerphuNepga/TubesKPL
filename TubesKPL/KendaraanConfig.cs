cc76611dc54af101fb6de76c5a724c95b0ab11f7
M Arrayan <fighterfly9@gmail.com>
2025-04-24 19:47:24 +0700

    menambahkan MetodePembayaran dan KendaraanConfig dengan menggunakan teknik runtime config dan generic

---
+++ TubesKPL/KendaraanConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TubesKPL
+{
+    internal class KendaraanConfig
+    {
+        public ACuciKendaraan aCuciConfig;
+        public const string filepath = @"kendaraan_config.json";
+        public ACuciKendaraan ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filepath);
+            aCuciConfig = JsonSerializer.Deserialize<ACuciKendaraan>(configJsonData);
+            return aCuciConfig;
+        }
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            String jsonString = JsonSerializer.Serialize(aCuciConfig);
+            File.WriteAllText(filepath, jsonString);
+        }
+        private void SetDefault()
+        {
+            aCuciConfig = new ACuciKendaraan("Mobil Default", "Mobil");
+        }
+
+        public KendaraanConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+    }
+}
